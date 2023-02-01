92baf15ea77ddf454cccbb9708de4e7bfdd7a6d1
TimeBather <open-source@timebather.cn>
2023-02-01 17:27:07 +0800

    Chore : remove the debug code

--- src/service.ts
+++ src/service.ts
@@ -6,7 +6,6 @@ export class BlocklyService extends Service{
   }
   async json_parse(v,t){
     try {
-      console.info(v,t)
       return await JSONPath({path:t,json:v})
     }catch(e){
       this.ctx.logger('blockly-lib').warn("Failed to read the object:"+e.toString())
