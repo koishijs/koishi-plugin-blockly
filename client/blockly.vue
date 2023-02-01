e40a34a2dd1e712f18704abd1137e84ad721a44d
TimeBather <open-source@timebather.cn>
2023-02-01 13:20:44 +0800

    fix : remove the zoom because the large blocks in the toolbox

--- client/blockly.vue
+++ client/blockly.vue
@@ -35,14 +35,6 @@ onMounted(() => {
   workspace = Blockly.inject(blockly_workspace.value, {
       toolbox:Toolbox,
       media : '/static/blockly/',
-      zoom : {
-        controls : true,
-        wheel : true,
-        startScale : 1,
-        maxScale : 3,
-        minScale : 0.3,
-        scaleSpeed : 1.2
-      },
       grid : {
         spacing : 20,
         length : 1,
