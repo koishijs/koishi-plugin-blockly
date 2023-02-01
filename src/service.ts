06c61c0fdf8d130e6ad0ff5a1afcd62fa84f0e13
TimeBather <open-source@timebather.cn>
2023-02-01 17:25:59 +0800

    feat : use jsonpath-plus instead of jsonpath

--- src/service.ts
+++ src/service.ts
@@ -1,12 +1,13 @@
 import {Service} from "koishi";
-import jsonPath from 'jsonpath'
+import {JSONPath} from 'jsonpath-plus';
 export class BlocklyService extends Service{
   constructor(ctx) {
     super(ctx,'blockly');
   }
   async json_parse(v,t){
-    try{
-      return await jsonPath.query(v,t)
+    try {
+      console.info(v,t)
+      return await JSONPath({path:t,json:v})
     }catch(e){
       this.ctx.logger('blockly-lib').warn("Failed to read the object:"+e.toString())
       return undefined
