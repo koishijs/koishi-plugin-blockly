d5416c9c88b3ca1dd4512bf21aeed8a9e71f28fc
MCBBC <linayunli@outlook.com>
2023-02-12 02:46:50 +0800

    Fix: Optimize code

--- client/blocks/text.ts
+++ client/blocks/text.ts
@@ -1,34 +1,30 @@
 import {javascriptGenerator} from "blockly/javascript";
 export const RegularExp = {
-  "type": "regular_exp",
-  "message0": "表达式 %1 修饰符 %2 %3",
-  "args0": [
-    {
-      "type": "field_input",
-      "name": "regular",
-      "text": ".*"
-    },
-    {
-      "type": "field_input",
-      "name": "modifier",
-      "text": "m"
-    },
-    {
-      "type": "input_value",
-      "name": "text"
-    }
-  ],
-  "output": null,
-  "colour": 120,
-  "tooltip": "",
-  "helpUrl": ""
-}
+    "type": "regular_exp",
+    "message0": "正则表达式 %1 %2",
+    "args0": [
+      {
+        "type": "field_input",
+        "name": "regular",
+        "text": "/.*/g"
+      },
+      {
+        "type": "input_value",
+        "name": "str"
+      }
+    ],
+    "output": null,
+    "colour": 160,
+    "tooltip": "",
+    "helpUrl": ""
+  }
 
 export function regularExp(block) {  
   let text_regular = block.getFieldValue('regular');
-  let text_modifier = block.getFieldValue('modifier');
-  let value_text = javascriptGenerator.valueToCode(block, 'text', javascriptGenerator.ORDER_ATOMIC);
-  return [`${ value_text }.match(new RegExp("${ text_regular }", "${ text_modifier }"))`, javascriptGenerator.ORDER_NONE];
+  let value_text = javascriptGenerator.valueToCode(block, 'str', javascriptGenerator.ORDER_ATOMIC);
+  let reg_exp = new RegExp("\/(.*)\/([g|m|i|y|u|s|d]+)", "g").exec(text_regular);
+  value_text = value_text.replace(/[|\\{}()[\]^$+*?.]/g, '\\$&').replace(/-/g, '\\x2d');
+  return [`${ value_text }.match(new RegExp("${ reg_exp[1] }", "${ reg_exp[2] }"))`, javascriptGenerator.ORDER_NONE];
 }
 
 export const TextBlocks = [
