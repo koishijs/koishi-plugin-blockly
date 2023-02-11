8cb151b6a7e342727728a37cff33d494a3c54109
TimeBather <open-source@timebather.cn>
2023-02-11 17:56:29 +0800

    fix : fix #25 cannot auto save

--- client/listeners/auto-save.ts
+++ client/listeners/auto-save.ts
@@ -3,6 +3,6 @@ import {BlockMove} from "blockly/core/events/events_block_move";
 import {BlockCreate} from "blockly/core/events/events_block_create";
 
 export function autoSaveListener(this:{autoSave:Function},_event:Abstract){
-  if (!(_event.type === 'move'))return;
+  if (!(_event.type === 'create' || _event.type === 'move'  || _event.type === 'delete' || _event.type === 'change'))return;
   this.autoSave();
 }
