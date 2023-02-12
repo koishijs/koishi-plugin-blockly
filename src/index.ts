1cf7514b87ca39a464f4a4c55badc4668a04bd15
TimeBather <open-source@timebather.cn>
2023-02-12 14:27:25 +0800

    fix : do not reload when the code did not change

--- src/index.ts
+++ src/index.ts
@@ -123,7 +123,7 @@ export async function apply(ctx: Context) {
     if(data.name)save_object['name'] = data.name
     save_object ['edited'] = !data.code
     await ctx.database.set("blockly",id,save_object);
-    setTimeout(()=>updatePmPlugins(ctx),0);
+    setTimeout(()=>updatePmPlugins(ctx,!!data.code),0);
     //console.info(save_object)
   },{authority:5})
   const logger = ctx.logger('logger1')
@@ -161,10 +161,12 @@ export async function apply(ctx: Context) {
 
   let pm = new PluginManager(ctx.isolate([]))
 
-  async function updatePmPlugins(ctx:Context){
-    pm.plugins = (await ctx.database.get('blockly',{enabled:true},["code","enabled"]))
-      .filter(t=>t.enabled).map(t=>t.code)
-    pm.restart()
+  async function updatePmPlugins(ctx:Context,restart=true){
+    if(restart){
+      pm.plugins = (await ctx.database.get('blockly',{enabled:true},["code","enabled"]))
+        .filter(t=>t.enabled).map(t=>t.code)
+      pm.restart()
+    }
     if(ctx['console.blockly']){
       await ctx['console.blockly'].refresh()
     }
