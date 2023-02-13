80cb27bf12cc5b394a087b472c7fe16e3cb04440
TimeBather <open-source@timebather.cn>
2023-02-14 01:52:59 +0800

    fix : fix highlight no-effect

--- client/components/console/code.vue
+++ client/components/console/code.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
 import {defineProps} from 'vue'
 import 'highlight.js/styles/stackoverflow-light.css'
-import 'highlight.js/lib/common';
+import highlight from 'highlight.js/lib/common';
+import HighlightJsRender from 'highlight.js/lib/languages/javascript'
+highlight.registerLanguage("javascript",HighlightJsRender)
 import HighlightVue from "@highlightjs/vue-plugin";
 const HighLight = HighlightVue.component
 const props = defineProps([
@@ -11,7 +13,7 @@ const props = defineProps([
 </script>
 <template>
   <template v-if="blocklyInformation.code">
-    <high-light :code="blocklyInformation.code"></high-light>
+    <high-light :code="blocklyInformation.code" lang="javascript"></high-light>
   </template>
 </template>
 
