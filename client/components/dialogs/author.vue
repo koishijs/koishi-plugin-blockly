b9eb29fad95e2ba92233e4364853b14ab7a4ddc7
TimeBather <open-source@timebather.cn>
2023-02-18 20:50:59 +0800

    feat: add verification and placeholder for author field

--- client/components/dialogs/author.vue
+++ client/components/dialogs/author.vue
@@ -13,6 +13,17 @@ const $emit = defineEmits(['update:modelValue','callback'])
 const visible = ref<any>(false)
 
 function execute(){
+  if(!content.value.name || !content.value.email){
+    return
+  }
+  if(!content.value.name.match(/^[A-Za-z0-9_-]+$/)){
+    alert('作者名称只能包含英文、数字、下划线和中划线')
+    return
+  }
+  if(!content.value.email.match(/^[A-Za-z0-9_-]+@[A-Za-z0-9_\-.]+$/)){
+    alert('作者邮箱格式不正确')
+    return
+  }
   $emit('update:modelValue',false)
   $emit('callback',content.value.name+'<'+content.value.email+'>')
   content.value.name = undefined
@@ -39,8 +50,8 @@ watch(visible,(newValue)=>{
 </script>
 <template>
   <el-dialog v-model="visible" title="设置作者">
-    <el-input v-model="content.name" label="作者名称(请使用英文名称)"></el-input>
-    <el-input v-model="content.email" label="作者邮箱(请使用英文邮箱)"></el-input>
+    <el-input v-model="content.name" placeholder="作者名称(请使用英文名称)" ></el-input>
+    <el-input v-model="content.email" placeholder="作者邮箱"></el-input>
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="$emit('update:modelValue')">关闭</el-button>
