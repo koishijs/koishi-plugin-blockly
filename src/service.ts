6c3f2751993a5d3bf5daeb84c97f9188976d1cd7
TimeBather <open-source@timebather.cn>
2023-02-18 15:47:02 +0800

    chore : remove the "jsonpath" and the "date format" api from blockly's service

--- src/service.ts
+++ src/service.ts
@@ -1,6 +1,4 @@
 import {Service} from "koishi";
-import {JSONPath} from 'jsonpath-plus';
-import {Time} from "koishi"
 import {PluginManager} from "./plugin";
 
 declare module "koishi"{
@@ -16,18 +14,6 @@ export class BlocklyService extends Service{
   constructor(ctx) {
     super(ctx,'blockly');
     this.manager = new PluginManager(ctx)
-  }
-  async json_parse(v,t){
-    try {
-      return await JSONPath({path:t,json:v})
-    }catch(e){
-      this.ctx.logger('blockly-lib').warn("Failed to read the object:"+e.toString())
-      return undefined
-    }
-  }
-
-  async date_format(t, f){
-    return Time.template(f,t)
   }
 
   async reload(restart?:boolean){
