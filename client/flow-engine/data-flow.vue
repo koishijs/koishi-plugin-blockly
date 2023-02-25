363ed7e3e4f19d564facdd7bc65c44a9463c8501
TimeBather <open-source@timebather.cn>
2023-02-26 03:37:10 +0800

    feat : the editor state syncing for flow editor with blockly

--- client/flow-engine/data-flow.vue
+++ client/flow-engine/data-flow.vue
@@ -1,24 +1,41 @@
 <template>
   <div style="height: 100%;width: 100%">
     <div style="width: 100%;padding: 5px;padding-left: 20px;height:25px">
-      <button class="menu-button">退出编辑模式</button>
+      <button class="menu-button" @click="exitAndSave">退出编辑模式</button>
+
       </div>
     <div style="width:100%;height: 100%"><div style="height: 100%;" ref="baklava_workspace"></div></div>
   </div>
 </template>
 <script lang="ts" setup>
-import {onMounted, ref,unref} from "vue";
+import {onMounted, ref,unref,watch,toRef} from "vue";
 
+let $emit = defineEmits(['update:workspace','update:flow'])
+let props = defineProps(['flow'])
+let flow = toRef(props,'flow')
+let baklavaInstance = null
 const baklava_workspace = ref(null)
 import {createBaklava} from "baklavajs/dist/esm/bundle";
 import "@baklavajs/themes/dist/classic.css"
 import {Nodes} from "./nodes";
+import {IEditorState} from "baklavajs";
 
 onMounted(()=>{
   const baklava = createBaklava(unref(baklava_workspace))
-  console.info(baklava)
+  const emptyPlugin = baklava.editor.save()
+  baklavaInstance = baklava
   Nodes.forEach(t=>baklava.editor.registerNodeType(t[0],{category:t[1]}))
+  watch(flow,(flow:IEditorState)=>{
+    if(flow.graph && flow.graphTemplates)
+      baklava.editor.load(flow)
+    else
+      baklava.editor.load(emptyPlugin)
+  })
 })
+function exitAndSave(){
+  $emit('update:flow',baklavaInstance.editor.save())
+  $emit('update:workspace','blockly')
+}
 
 </script>
 
