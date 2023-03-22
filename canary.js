56fded780279e618037938e91cd2f7503a6881e5
TimeBather <open-source@timebather.cn>
2023-03-22 16:32:15 +0800

    fix: run canary script only in the canary branch

--- canary.js
+++ canary.js
@@ -8,9 +8,12 @@ const {execSync} = require('child_process');
 async function publish(){
   const packageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
   const originalPackageJson = JSON.parse(fs.readFileSync('package.json', 'utf8'));
+  if(!packageJson.name.endsWith('-canary')){
+    console.error('This script should only be run on canary packages')
+    return;
+  }
   const version = packageJson.version;
   packageJson.version = version + '-canary.' + Date.now()
-  packageJson.name = packageJson.name + '-canary'
   fs.writeFileSync('package.json', JSON.stringify(packageJson, null, 2))
   try{
     execSync('yarn publish --access public', {stdio: 'inherit'});
